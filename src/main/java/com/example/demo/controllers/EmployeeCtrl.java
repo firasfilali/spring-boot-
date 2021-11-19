a1ca77e92da38d2be14fefdb5a373092497977d9
firasfilali <76403765+firasfilali@users.noreply.github.com>
2021-11-20 00:31:49 +0100

    Update EmployeeCtrl.java

--- src/main/java/com/example/demo/controllers/EmployeeCtrl.java
+++ src/main/java/com/example/demo/controllers/EmployeeCtrl.java
@@ -1,6 +1,9 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,10 @@ public class EmployeeCtrl {
 	@PostMapping("/employee/add")
 	public EmployeeEntity saveEmployee(@RequestBody EmployeeEntity employeeEntity) {
 		return empService.saveToDB(employeeEntity);
+	}
+	
+	@GetMapping("/employee/get")
+	public List<EmployeeEntity> getAllemp(){
+		return empService.getAllEmployee();
 	}
 }
