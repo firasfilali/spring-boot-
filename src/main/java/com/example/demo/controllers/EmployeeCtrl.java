905d74d30dfb2614d2147e6cb032f873d252b9a5
firasfilali <76403765+firasfilali@users.noreply.github.com>
2021-11-30 00:06:11 +0100

    Exception Handler

--- src/main/java/com/example/demo/controllers/EmployeeCtrl.java
+++ src/main/java/com/example/demo/controllers/EmployeeCtrl.java
@@ -1,8 +1,12 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +35,11 @@ public class EmployeeCtrl {
 	@GetMapping("/employee/get/{id}")
 	public EmployeeEntity getOneemp(@PathVariable int id){
 		return empService.getEmpById(id);
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNSEE(NoSuchElementException e){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error in search : " + e.getMessage());
 	}
 	
 }
